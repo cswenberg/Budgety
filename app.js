7ad597650eebb0fc352157eb1c871de8973fb3c5
cswenberg <cls364@cornell.edu>
2018-06-29 19:38:24 -0700

    pausing for now

---
+++ app.js
@@ -0,0 +1,45 @@
+
+
+// data module
+var budgetController = (function() {
+
+
+
+
+	return {
+		//All publicly available methods as attribute of return object
+
+
+	}
+})()
+
+
+// ui module
+var uiController = (function() {
+
+
+
+
+	return {
+		//All publicly available methods as attribute of return object
+
+
+	}
+})()
+
+
+
+// controller module
+var controller = (function(budgetCtrl, uiCtrl) {
+
+
+
+
+	return {
+		//All publicly available methods as attribute of return object
+
+
+	}
+})(budgetController, uiController)
+
+
