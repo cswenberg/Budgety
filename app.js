dc84b4394697f7a6a943a6d2240f8a0e559d6d74
cswenberg <cls364@cornell.edu>
2018-07-04 11:23:25 -0700

    UI updates and budget calculations

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
 // data module
 var budgetController = (function() {
 
-	var Item = function(id, des, val) {
+	var Item = function(id, desc, val) {
 		this.id = id
-		this.description = des
+		this.description = desc
 		this.value = val
 	}
 
@@ -24,34 +24,44 @@ var budgetController = (function() {
 		},
 		expense: {
 			list: [],
-			total: 0
-		}
+			total: 0,
+			percentage: 0
+		},
+		budget: 0
+
 	}
 
 	var updateBudget = function() {
-		var newBudget = document.querySelector(DOM.income).value + document.querySelector(DOM.expenses).value
-		console.log(newBudget)
+		
 	}
 
 	return {
 		//All publicly available methods as attribute of return object
 		addItem: function(type, description, value) {
-			var num
-			if (type == 'income') {
-				num = Number(value)
-			} else if (type == 'expense') {
-				num = value * -1
-			}
 			var id
+
 			if (data[type].list.length == 0) {id = 0}
 			else {id = data[type].list[data[type].list.length-1].id+1}
-			var item = new Item(id, description, num)
+			var item = new Item(id, description, value)
 
 			data[type].list.push(item)
 			data[type].total += item.value
-			console.log(data)
+			//console.log(data)
+			return item
+		},
 
-			//updateBudget()
+		updateBudget: function() {
+			data.budget = data.income.total - data.expense.total
+			if (data.income.total>0) {data.expense.percentage = Math.round(100 * data.expense.total / data.income.total)}
+		},
+
+		getBudget: function() {
+			return {
+				income: data.income.total,
+				expenses: data.expense.total,
+				budget: data.budget,
+				percentage: data.expense.percentage
+			}
 		}
 
 	}
@@ -65,7 +75,14 @@ var uiController = (function() {
 		type: '.add__type',
 		description: '.add__description',
 		value: '.add__value',
-		button: '.add__btn'
+		button: '.add__btn',
+		incomeList: '.income__list',
+		expensesList: '.expenses__list',
+		budgetLabel: '.budget__value',
+		incomeLabel: '.budget__income--value',
+		expensesLabel: '.budget__expenses--value',
+		percentageLabel: '.budget__expenses--percentage',
+		container: '.container'
 	}
 
 
@@ -77,7 +94,54 @@ var uiController = (function() {
 			return {
 				type: document.querySelector(DOM.type).value,
 				description: document.querySelector(DOM.description).value,
-				value: document.querySelector(DOM.value).value,
+				value: parseFloat(document.querySelector(DOM.value).value)
+			}
+		},
+		addItem: function(obj, type) {
+			var html, list
+
+			if (type == 'income') {
+				list = DOM.incomeList
+				html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"> <i class="ion-ios-close-outline"> </i></button> </div> </div> </div>'
+				html = html.replace('%value%', '+ ' + obj.value)
+			} else if (type == 'expense') {
+				list = DOM.expensesList
+				html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">%percentage%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"> </i></button> </div> </div> </div>';
+				html = html.replace('%value%', '- ' + obj.value)
+			}
+			//populate placeholders with object's actual data
+			html = html.replace('%id%', obj.id) 
+			html = html.replace('%description%', obj.description)
+
+			document.querySelector(list).insertAdjacentHTML('beforeend', html) //input this html text at the end of the income or expenses list block
+
+		},
+
+		resetFields: function() {
+
+			var fields = document.querySelectorAll(DOM.description+', '+DOM.value) //returns a list of elements, not an array
+
+			var array = Array.prototype.slice.call(fields) //forces call of Array's method slice on our list fields to return an array of elements
+
+			array.forEach(function(element) {
+				element.value = '' //sets element's text value to empty string to clear it
+			})
+
+			array[0].focus() //puts cursor back on DOM.description
+		},
+
+		displayBudget: function(obj) {
+			var budg
+			if (obj.budget>0) {budg = '+ ' + obj.budget} 
+			else if (obj.budget<0) {budg = '- ' + Math.abs(obj.budget)}
+			else {budg = 0}
+			document.querySelector(DOM.budgetLabel).textContent = budg
+			document.querySelector(DOM.incomeLabel).textContent = '+ ' + obj.income
+			document.querySelector(DOM.expensesLabel).textContent = '- ' + obj.expenses
+			if (obj.percentage>0) {
+				document.querySelector(DOM.percentageLabel).textContent = obj.percentage + '%'
+			} else {
+				document.querySelector(DOM.percentageLabel).textContent = '---'
 			}
 
 		}
@@ -101,14 +165,41 @@ var controller = (function(budget, ui) {
 				addItem()
 			}
 		})
+		document.querySelector(DOM.container).addEventListener('click', deleteItem)
 	}
 
-
 	var addItem = function() {
-
+		//gather fields
 		var inputs = ui.getInputs()
 
-		budget.addItem(inputs.type, inputs.description, inputs.value)
+		if (inputs.description!='' && !isNaN(inputs.value) && inputs.value>0) {
+			//create new item
+			var item = budget.addItem(inputs.type, inputs.description, inputs.value)
+			//display new item on ui
+			ui.addItem(item, inputs.type)
+			//clear input values and refocus cursor 
+			ui.resetFields()
+			//update budget and display on ui
+			updateBudget()
+		}
+	}
+
+	var deleteItem = function(event) {
+
+		var itemID = event.target.parentNode.parentNode.parentNode.parentNode.id
+		if (itemID) {
+			var split = itemID.split('-')
+			var type = split[0]
+			var id = split[1]
+		}
+
+	}
+
+	var updateBudget = function() {
+
+		budget.updateBudget()
+		var newBudget = budget.getBudget()
+		ui.displayBudget(newBudget)
 
 	}
 
@@ -116,6 +207,12 @@ var controller = (function(budget, ui) {
 		//All publicly available methods as attribute of return object
 		init: function() {
 			createListeners()
+			ui.displayBudget({
+				income: 0,
+				expenses: 0,
+				budget: 0,
+				percentage: -1
+			})
 		}
 
 	}
